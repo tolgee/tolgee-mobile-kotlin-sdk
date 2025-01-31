fa11ec005b3ade8d0c2e9bebecf6371b075a42d4
DatLag <jeff.retz08@gmail.com>
2025-01-31 01:31:20 +0100

    added Context extension function on android

--- core/src/androidMain/kotlin/dev/datlag/tolgee/TolgeeAndroid.kt
+++ core/src/androidMain/kotlin/dev/datlag/tolgee/TolgeeAndroid.kt
@@ -8,15 +8,23 @@ data class TolgeeAndroid internal constructor(
     override val config: Config
 ) : Tolgee(config) {
 
-    fun getString(context: Context, @StringRes key: Int, vararg args: Any): String {
+    suspend fun getString(context: Context, @StringRes key: Int, vararg args: Any): String {
+        return getKeyFromRes(context, key)?.let {
+            getTranslation(key = it, args = args)
+        } ?: getStringFromCache(context, key, *args)
+    }
+
+    fun getStringFromCache(context: Context, @StringRes key: Int, vararg args: Any): String {
         return getKeyFromRes(context, key)?.let {
             getTranslationFromCache(key = it, args = args)
         } ?: context.getString(key, *args)
     }
 
-    private fun getKeyFromRes(context: Context, @StringRes key: Int): String? {
-        return scopeCatching {
-            context.resources.getResourceEntryName(key)
-        }.getOrNull()?.trim()?.ifBlank { null }
+    companion object {
+        internal fun getKeyFromRes(context: Context, @StringRes key: Int): String? {
+            return scopeCatching {
+                context.resources.getResourceEntryName(key)
+            }.getOrNull()?.trim()?.ifBlank { null }
+        }
     }
 }
--- core/src/androidMain/kotlin/dev/datlag/tolgee/common/ExtendTolgee.android.kt
+++ core/src/androidMain/kotlin/dev/datlag/tolgee/common/ExtendTolgee.android.kt
@@ -1,5 +1,7 @@
 package dev.datlag.tolgee.common
 
+import android.content.Context
+import androidx.annotation.StringRes
 import dev.datlag.tolgee.Tolgee
 import dev.datlag.tolgee.TolgeeAndroid
 import io.ktor.client.*
@@ -17,3 +19,22 @@ internal actual fun createPlatformTolgee(config: Tolgee.Config): Tolgee {
 
 internal actual val platformNetworkContext: CoroutineContext
     get() = Dispatchers.IO
+
+/**
+ * Returns a localized formatted string from [Tolgee] cache or the application's package's
+ * default string table, substituting the format arguments as defined in
+ * [java.util.Formatter] and [java.lang.String.format].
+ *
+ * @param tolgee The [Tolgee] instance to get the cached string from.
+ * @param resId Resource id for the format string
+ * @param formatArgs The format arguments that will be used for
+ *                   substitution.
+ * @return The string data associated with the resource, formatted and
+ *         stripped of styled text information.
+ */
+fun Context.getString(tolgee: Tolgee, @StringRes resId: Int, vararg formatArgs: Any): String {
+    return (tolgee as? TolgeeAndroid)?.getStringFromCache(this, resId, *formatArgs)
+        ?: TolgeeAndroid.getKeyFromRes(this, resId)?.let {
+        tolgee.getTranslationFromCache(key = it, args = formatArgs)
+    } ?: this.getString(resId, *formatArgs)
+}
