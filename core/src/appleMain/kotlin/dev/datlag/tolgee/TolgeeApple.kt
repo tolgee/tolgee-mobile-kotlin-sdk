84f92d567c123b8af44a84cf9dea4df1c40334ec
DatLag <jeff.retz08@gmail.com>
2025-02-08 17:22:20 +0100

    fix building on apple

--- core/src/appleMain/kotlin/dev/datlag/tolgee/TolgeeApple.kt
+++ core/src/appleMain/kotlin/dev/datlag/tolgee/TolgeeApple.kt
@@ -2,6 +2,8 @@ package dev.datlag.tolgee
 
 import dev.datlag.tolgee.common.mapNotNull
 import dev.datlag.tolgee.model.TolgeeMessageParams
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ObjCObject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
@@ -43,9 +45,10 @@ data class TolgeeApple internal constructor(
                 }?.localizedStringForKey(key, default?.ifBlank { null }, table).takeUnless { it == key }?.ifBlank { null }
         }
 
+        @OptIn(BetaInteropApi::class)
         fun getLocalizedStringFromBundleFormatted(key: String, default: String?, table: String?, vararg args: Any): String? {
             return (getLocalizedStringFromBundle(key, default, table) ?: default?.ifBlank { null })?.let { format ->
-                NSString.localizedStringWithFormat(format, *args.toList().toTypedArray())
+                NSString.localizedStringWithFormat(format, *args.map { it as? ObjCObject }.toTypedArray())
             }
         }
     }
