df9b8ba79a5953b49ffe96f805f9857d5dbaf34e
DatLag <jeff.retz08@gmail.com>
2025-04-03 17:35:22 +0200

    added ContextWrapper to resolve getString even without compiler plugin

---
+++ core/src/androidMain/kotlin/dev/datlag/tolgee/TolgeeContextWrapper.kt
@@ -0,0 +1,75 @@
+package dev.datlag.tolgee
+
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.res.Resources
+import kotlinx.atomicfu.atomic
+
+/**
+ * A custom [ContextWrapper] that overrides string retrieval to fetch translations from the Tolgee platform.
+ *
+ * This class intercepts calls to [getString] and attempts to load the string from Tolgee.
+ * If no translation is loaded or found, it falls back to the default [Context.getString] implementation.
+ *
+ * @param base The base [Context] to wrap.
+ * @param tolgee The Tolgee translation service used for retrieving localized strings.
+ */
+class TolgeeContextWrapper(
+    val base: Context,
+    val tolgee: Tolgee
+) : ContextWrapper(base) {
+
+    private val res = atomic<Resources?>(null)
+
+    override fun getResources(): Resources? {
+        if (res.value == null) {
+            val superResources: Resources? = super.getResources()
+
+            if (superResources != null) {
+                res.compareAndSet(null, TolgeeResources(
+                    baseContext = base,
+                    base = superResources,
+                    tolgee = tolgee
+                ))
+            }
+
+        }
+        return res.value ?: super.getResources()
+    }
+
+    companion object {
+        /**
+         * Wraps the given [base] context with a [TolgeeContextWrapper] that uses the global singleton instance of Tolgee.
+         *
+         * This method is a convenience function for cases where `attachBaseContext` provides a nullable context.
+         * If [base] is `null`, it falls back to returning a regular [ContextWrapper].
+         *
+         * @param base The context to wrap, which may be `null`.
+         * @return A wrapped [ContextWrapper] with Tolgee support, or a regular [ContextWrapper] if Tolgee is unavailable.
+         */
+        @JvmStatic
+        fun wrap(base: Context?): ContextWrapper {
+            val tolgee = Tolgee.instance ?: return ContextWrapper(base)
+            return wrap(base, tolgee)
+        }
+
+        /**
+         * Wraps the given [base] context with a [TolgeeContextWrapper] using the provided [tolgee] instance.
+         *
+         * This method allows explicitly specifying a Tolgee instance for cases where dependency injection
+         * or multiple Tolgee instances are needed.
+         * If [base] is `null`, it falls back to returning a regular [ContextWrapper].
+         *
+         * @param base The context to wrap, which may be `null`.
+         * @param tolgee The Tolgee translation service instance to use for retrieving localized strings.
+         * @return A wrapped [ContextWrapper] with Tolgee support, or a regular [ContextWrapper] if [base] is `null`.
+         */
+        @JvmStatic
+        fun wrap(base: Context?, tolgee: Tolgee): ContextWrapper {
+            if (base == null) {
+                return ContextWrapper(base)
+            }
+            return TolgeeContextWrapper(base, tolgee)
+        }
+    }
+}
--- demo/android-view/src/main/kotlin/io/tolgee/demo/view/MainActivity.kt
+++ demo/android-view/src/main/kotlin/io/tolgee/demo/view/MainActivity.kt
@@ -1,11 +1,13 @@
 package io.tolgee.demo.view
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.lifecycleScope
 import dev.datlag.tolgee.Tolgee
+import dev.datlag.tolgee.TolgeeContextWrapper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -13,6 +15,18 @@ import kotlinx.coroutines.withContext
 import java.util.Locale
 
 class MainActivity : ComponentActivity() {
+
+    val tolgee = Tolgee.instanceOrInit {
+        contentDelivery {
+            id(System.getenv("TOLGEE_API_KEY"))
+            formatter(Tolgee.Formatter.Sprintf)
+        }
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(TolgeeContextWrapper.wrap(newBase))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,13 +35,6 @@ class MainActivity : ComponentActivity() {
         val basic = findViewById<TextView>(R.id.basic_text)
         val parameter = findViewById<TextView>(R.id.parameterized_text)
         val button = findViewById<Button>(R.id.button)
-
-        val tolgee = Tolgee.instanceOrInit {
-            contentDelivery {
-                id(System.getenv("TOLGEE_API_KEY"))
-                formatter(Tolgee.Formatter.Sprintf)
-            }
-        }
 
         lifecycleScope.launch {
             tolgee.translation(
